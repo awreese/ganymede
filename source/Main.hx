05171665e1f95a5ee3ef521e442a01b46a1d1019
Weirdbob95 <rory.soiffer@comcast.net>
2017-05-21 23:20:04 -0700

    Added more levels

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,7 @@ class Main extends Sprite
 {
 	public static var LOGGER: CapstoneLogger;
 	public static var LEVEL: Int;
-	public static inline var FINAL_LEVEL: Int = 5;
+	public static inline var FINAL_LEVEL: Int = 8;
 	public function new()
 	{
 		LEVEL = 1;
