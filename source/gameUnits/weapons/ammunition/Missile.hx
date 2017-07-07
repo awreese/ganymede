77342c2d21c601fb9cac831b1e620f2710e680a3
Drew Reese <drew.w.reese@gmail.com>
2017-07-06 22:49:15 -0700

    Basic Missile behavior recovered.
    Only small missiles for now, but target tracking seems to be functional
    once again.  Need to create missile interface to ensure missile exhaust
    particle systems get started if included.

--- source/gameUnits/weapons/ammunition/Missile.hx
+++ source/gameUnits/weapons/ammunition/Missile.hx
@@ -22,18 +22,34 @@ import flixel.addons.weapon.FlxBullet;
 import flixel.addons.weapon.FlxWeapon.FlxTypedWeapon;
 import flixel.addons.weapon.FlxWeapon.FlxWeaponFireFrom;
 import flixel.addons.weapon.FlxWeapon.FlxWeaponSpeedMode;
+import flixel.math.FlxAngle;
+import flixel.math.FlxPoint;
+import flixel.math.FlxVector;
+import flixel.math.FlxVelocity;
+import flixel.util.helpers.FlxBounds;
 import gameUnits.Ship;
+import gameUnits.weapons.WeaponSize;
 
 /**
  * Missiles are used for launchers.
  * @author Drew Reese
  */
 class Missile extends Ammunition  {
+    
+    public static var LAUNCH_SPEED:FlxBounds<Float> = new FlxBounds(30.0);
+    
+    private var _velocity:Float;
+    private var _acceleration:Float;
 	
-	private function new(damage:Float)  {
-		super(damage);
+	private function new(missileSize:WeaponSize, damage:Float, velocity:Float, acceleration:Float)  {
+		super(missileSize, damage);
 		this.accelerates = true;
         
+        this._velocity = velocity;
+        this._acceleration = acceleration;
+        
+        loadGraphic(AssetPaths.missile__png, true, 61, 9);
+        animation.add("fire", [1, 2, 3, 2], 2, true);
 	}
 	
 	override public function update(elapsed:Float):Void {
@@ -43,16 +59,57 @@ class Missile extends Ammunition  {
 		if (target.exists) {
 			
 			// adjust trajectory to target
+            adjustTrajectory();
 			
 			// check for collision
 			if (this.overlaps(target)) {
 				// Do damage to target & destroy missile
-				target.hurt(damage);
+				target.hurt(_damage);
 				// maybe show explosion animation?
 				this.kill();
 			}
 		}
+        
+        adjustAngle();
 		
 	}
+    
+    public function start(a:Bool, b:Float):Void {
+        
+    }
+    
+    private function adjustTrajectory():Void {
+        var p1 = this.getMidpoint();
+        var p2 = target.getMidpoint();
+        
+        var dx = p2.x - p1.x;
+        var dy = p2.y - p1.y;
+        
+        p1.put();
+        p2.put();
+        
+        var angle = Math.atan2(dy, dx);
+        
+        FlxVelocity.accelerateFromAngle(this, angle, _acceleration, _velocity, false);
+    }
+    
+    private function adjustAngle():Void {
+        this.angle = pointToVector(this.velocity).degrees;
+    }
+    
+    private static inline function pointToVector(point:FlxPoint):FlxVector {
+        return FlxVector.get(point.x, point.y);
+    }
 	
+}
+
+class Missile_Small extends Missile {
+    public static var FLIGHT_TIME:FlxBounds<Float> = new FlxBounds(3.0);
+    public static var LAUNCH_SPEED:FlxBounds<Float> = new FlxBounds(30.0);
+    
+    public function new(?damage:Float = 5.0, ?velocity:Float = 120.0, ?acceleration:Float = 100.0) {
+        super(SMALL, damage, velocity, acceleration);
+        this.setGraphicSize(16, 0);
+        this.updateHitbox();
+    }
 }
