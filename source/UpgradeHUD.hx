966dbb6b433ecee83b4e74c78c1c1fa072e23698
Daisy <daisy1xu4@gmail.com>
2018-02-16 09:14:51 -0800

    started on upgrade hub

---
+++ source/UpgradeHUD.hx
@@ -0,0 +1,17 @@
+package;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.FlxSprite;
+
+/**
+ * ...
+ * @author Daisy Xu
+ */
+class UpgradeHUD extends FlxTypedGroup<FlxSprite>
+{
+	private var _screen:FlxSprite;
+	public function new() 
+	{
+		super();
+	}
+	
+}
