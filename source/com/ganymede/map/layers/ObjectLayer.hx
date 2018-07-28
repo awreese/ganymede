464103b376481f15b54e1a350347dae0ecad63c2
Drew Reese <drew.w.reese@gmail.com>
2018-07-27 19:03:27 -0700

    Layer Updates
    - Added layer descriptions
    - Fleshed out ObjectLayer a smidge

--- source/com/ganymede/map/layers/ObjectLayer.hx
+++ source/com/ganymede/map/layers/ObjectLayer.hx
@@ -1,9 +1,33 @@
+/**
+ *  Astrorush: TBD (The Best Defense)
+ *  Copyright (C) 2018 Andrew Reese
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.ganymede.map.layers;
 
+import com.ganymede.db.DB_Data;
 import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.group.FlxGroup;
-import js.Browser;
+
+private typedef NodeArray = Array<DB_LevelNode>;
+private typedef PlanetArray = Array<DB_LevelPlanet>;
+private typedef BeaconArray = Array<DB_LevelBeacon>;
+private typedef HazzardArray = Array<DB_LevelHazzard>;
+private typedef PowerupArray = Array<DB_LevelPowerup>;
 
 /**
  * Object layer holds instances of planet, beacon, and hazzard groups.
@@ -32,9 +56,9 @@ class ObjectLayer extends FlxGroup {
     this.planets = new FlxGroup();
     this.ships = new FlxGroup();
     
+    super.add(this.planets);
     super.add(this.beacons);
     super.add(this.hazzards);
-    super.add(this.planets);
     super.add(this.ships);
   }
   
@@ -47,4 +71,17 @@ class ObjectLayer extends FlxGroup {
     return null;
   }
   
+  public function set_object_data(levelData:LevelData) {
+    var nodes:NodeArray = levelData.nodes;
+    
+    this.addPlanets(nodes, levelData.planets);
+  }
+  
+  private function addPlanets(nodes:NodeArray, planets:PlanetArray):Void {
+    //trace('Planets', planets);
+    
+    for (planet in planets) {
+      trace('Planet', planet, nodes[planet.nodeId]);
+    }
+  }
 }
