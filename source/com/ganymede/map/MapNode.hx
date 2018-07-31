477c4df41b173f1958cc66aba0a30f2a0cf0f609
Drew Reese <drew.w.reese@gmail.com>
2018-07-31 11:53:00 -0700

    Added mouseHandler.  Fixed colors.

--- source/com/ganymede/map/MapNode.hx
+++ source/com/ganymede/map/MapNode.hx
@@ -20,6 +20,7 @@ package com.ganymede.map;
 
 import com.ganymede.gameUnits.capturable.Capturable;
 import com.ganymede.util.Colors;
+import com.ganymede.util.MouseHandler;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
@@ -32,18 +33,13 @@ import flixel.util.FlxColor;
 import flixel.util.FlxSpriteUtil;
 
 private class Consts {
-  public static inline var NODE_RADIUS:Int = 15;
+  public static inline var NODE_RADIUS:Int = 10;
 }
 
-//private class Colors {
-  ////public static var NODE_DISK = FlxColor.fromRGBFloat(0.5, 0.5, 0.5, 0.6);
-  //public static var NODE_DISK = FlxColor.MAGENTA;
-  //public static var NODE_SELECTED = FlxColor.fromRGBFloat(0.9, 0.4, 0.5, 0.5);
-  //public static var NODE_HIGHLIGHT = FlxColor.fromRGBFloat(0.5, 0.5, 0.5, 0.3);
-//}
-
-private class Styles {
-  public static var NODE_SELECTED: flixel.util.LineStyle = {color: Colors.DEEPSKYBLUE, thickness: 1};
+private class NodeColors {
+  public static var NODE_DISK = Colors.GRAY;
+  public static var NODE_SELECTED = Colors.DEEPSKYBLUE;
+  public static var NODE_HIGHLIGHT = Colors.GRAY;
 }
 
 /**
@@ -54,7 +50,7 @@ class MapNode extends FlxGroup {
 
   public var id(default, null):Int;
   public var position(default, null):FlxPoint;
-  
+
   // Fields
   private var radius:Int;
   private var node_disk:MapNodeRing;
@@ -72,49 +68,38 @@ class MapNode extends FlxGroup {
     this.id = id;
     this.position = new FlxPoint(x, y);
     this.radius = radius;
-    var diameter:Int = 2 * radius + 1;
-    //super(x, y, diameter, diameter);
+    var diameter:Int = 2 * radius;
     super();
 
-    this.node_disk = new MapNodeRing(x, y, radius, true, Colors.NODE_DISK);
-    this.node_selected = new MapNodeRing(x, y, radius + 1, false, Colors.NODE_SELECTED, Styles.NODE_SELECTED);
-    this.node_highlight = new MapNodeRing(x, y, radius * 2, false, Colors.GREY, 0.5);
+    this.node_disk = new MapNodeRing(x, y, radius, true, NodeColors.NODE_DISK, 0.75);
+    this.node_selected = new MapNodeRing(x, y, radius * 3, false, NodeColors.NODE_SELECTED, 0.5);
+    this.node_highlight = new MapNodeRing(x, y, radius * 3, false, NodeColors.NODE_HIGHLIGHT, 0.5);
 
     this.capturable = null;
-    
-    this.add(this.node_disk);
-    this.add(this.node_highlight);
+
     this.add(this.node_selected);
-    
-    //trace(this);
+    this.add(this.node_highlight);
+    this.add(this.node_disk);
+
   }
 
   override public function update(elapsed:Float):Void {
-
-    var mouse:FlxMouse = FlxG.mouse;
-    var mousePos:FlxPoint = mouse.getPosition();
-
-    this.isMouseover = mouseOver(node_highlight, mousePos);
-    //node_highlight.visible = mouseOver(node_highlight, mousePos);
-    node_highlight.visible = this.isMouseover;
-
-    if (mouse.justPressed) {
-      node_selected.visible = mouseOver(node_selected, mousePos);
-      this.isSelected = node_selected.visible;
-    }
     super.update(elapsed);
+
+    this.handleMouseEvents();
+
   }
-  
+
   public function equals(other:Dynamic):Bool {
     trace('testing mapNode equality');
     //trace(this);
     //trace(other);
-    
+
     if (this == other) {
       trace('same node');
       return true;
     }
-    
+
     if (Std.is (other, MapNode)) {
       trace('Checking node equality');
       other = cast(other, MapNode);
@@ -125,8 +110,13 @@ class MapNode extends FlxGroup {
     return false;
   }
 
-  private static function mouseOver(o:FlxObject, p:FlxPoint):Bool {
-    return o.getHitbox().containsPoint(p);
+  private function handleMouseEvents():Void {
+    this.isMouseover = node_highlight.isHovered;
+    this.isSelected = node_highlight.isLeftClicked;
+
+
+    node_highlight.visible = this.isMouseover && !this.isSelected;
+    node_selected.visible = this.isSelected;
   }
 
   /**
@@ -161,6 +151,11 @@ class MapNode extends FlxGroup {
 
 class MapNodeRing extends FlxSprite {
 
+  public var isHovered(default, null):Bool;
+  public var isLeftClicked(default, null):Bool;
+
+  private var mouseHandler:MouseHandler;
+
   public function new (
     x:Float, y:Float,
     ?radius:Int = Consts.NODE_RADIUS,
@@ -171,16 +166,32 @@ class MapNodeRing extends FlxSprite {
     ?drawStyle:DrawStyle
   ) {
     super(x, y);
-    
+
     opacity = FlxMath.bound(opacity, 0.0, 1.0);
 
-    var diameter:Int = 2 * radius + 1;
+    var diameter:Int = 2 * radius;
     this.makeGraphic(diameter, diameter, FlxColor.TRANSPARENT, true);
-    
+
     fill.alphaFloat = opacity;
 
     FlxSpriteUtil.drawCircle(this, -1, -1, radius, fill, lineStyle, drawStyle);
-    this.setPosition(x - (diameter * 0.5), y - (diameter * 0.5));
+    this.setPosition(x - radius, y - radius);
     this.visible = visible;
+
+    this.mouseHandler = new MouseHandler();
+    this.mouseHandler.addListener(MouseButton.LEFT, MouseEvent.UP, mouseDown);
+  }
+
+  override public function update(elapsed:Float):Void {
+    super.update(elapsed);
+
+    var mouse:FlxMouse = FlxG.mouse;
+    this.isHovered = mouse.overlaps(this);
+
+    this.mouseHandler.handleEvents();
+  }
+
+  private function mouseDown():Void {
+    this.isLeftClicked = this.isHovered;
   }
 }
